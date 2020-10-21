e8946846ff8b6f074a79dbf1adf9ca947673e4d7
Yacine A <yacine@MacBook-Pro-de-Yacine.local>
2020-10-21 21:40:31 +0200

    TP2

--- code/spark/tp1/src/main/scala/FlightData.scala
+++ code/spark/tp1/src/main/scala/FlightData.scala
@@ -3,7 +3,6 @@ import org.apache.spark.sql.{Row, SparkSession}
 
 object FlightData extends App {
 
-  case class Flight(DEST_COUNTRY_NAME: String, ORIGIN_COUNTRY_NAME: String, count: Int)
 
   // Instanciation du SparkSession
   val spark = SparkSession
@@ -50,14 +49,19 @@ object FlightData extends App {
   // Part 2
 
 
+  // on recupérer le schéma du DataFrame dans une variable
   val dfSchema = flight2010Json.schema
-  //dfSchema.foreach(println(_))
+  println("---- schema ---- ")
+  dfSchema.foreach(println(_))
 
+  // Creation d'un DataFrame à partir en parallélisant une collection de données
+  // On crée dans un premier temps le RDD ensuite on créé le DataFrame en inférant le schema manuellement
   val myRows = Seq(Row("Hello", null, 1L))
   val myRDD = spark.sparkContext.parallelize(myRows)
   val myDF = spark.createDataFrame(myRDD, dfSchema)
   // myDF.show()
 
+  // Faire une selection de différente manière
   val selectDF = flightData2015DF.select(
     col("DEST_COUNTRY_NAME"),
     column("DEST_COUNTRY_NAME"),
@@ -67,27 +71,36 @@ object FlightData extends App {
 
 
   //SelectExrp
+  // On ajoute une colone "local" au DF, la valeur de "local" est à true si le vol est local et false sinon
   val withinCountry = flightData2015DF
     .selectExpr("*", // take all column
       "DEST_COUNTRY_NAME = ORIGIN_COUNTRY_NAME as local" // add news column to DF where DEST=ORIGIN
     )
-  // withinCountry.show()
 
+   withinCountry.show()
+
+  // On filtre uniquement sur les vols locaux
   val localFlightsDF = withinCountry.where("local")
-  //localFlightsDF.show()
+  localFlightsDF.show()
 
-
+  /**
+   *  On selectionne deux colones :
+   *  1. on calcule la moyenne du nombre de vol
+   *  2. On compte le nombre de destination distinct
+   */
   val average = flightData2015DF
     .selectExpr("avg(count)", // moyenne des vols
       "count(distinct(DEST_COUNTRY_NAME))") // compter le nombre de pays dest distinct
 
-  //average.show()
+  average.show()
 
+  // On ajoute une colonne "local" de vol locaux, en appelant la fonction "withColumn"
   val local2 = flightData2015DF
     .withColumn("local",
       expr("DEST_COUNTRY_NAME=ORIGIN_COUNTRY_NAME"))
+  //local2.show()
 
-  // local2.show()
+  // Ajouter une nouvelle colonne où la DEST est en upper case
   val upperDF = flightData2015DF
     .withColumn("upper", upper(col("DEST_COUNTRY_NAME")))
 
@@ -112,36 +125,60 @@ object FlightData extends App {
   val dfOrder = flightData2015DF.orderBy(desc("count"))
   //dfOrder.show()
 
+  // compter le nombre d'élement du DataFrame
   val numberElem = flightData2015DF.count()
   println("number elem DF = " + numberElem)
 
-  //val somme = flightData2015DF.agg(sum("count"))
+   val somme = flightData2015DF.agg(sum("count"))
   //somme.show()
 
+  /**
+   * LES DATASETS
+   */
+
+  // on import spark.implicits pour convertir le DataFrame en Dataset
   import spark.implicits._
 
+  /**
+   * Créer la case Class Flight qui contient le schema du DataSet
+   */
+
+  case class Flight(DEST_COUNTRY_NAME: String, ORIGIN_COUNTRY_NAME: String, count: Int)
+
+  // Transformer le DataFrame en DataSet[Flight]
   val flightDS = flightData2015DF.as[Flight]
   // flightDS.show()
 
+  /**
+   *  Défénir une fonction qui prend un vol en entrée en renvoi true si le vol est local, false sinon
+   * @param vol : correspond à chaque ligne du DataSet
+   * @return : true si vol local, false sinon
+   */
   def localFlight(vol: Flight): Boolean = {
     vol.DEST_COUNTRY_NAME == vol.ORIGIN_COUNTRY_NAME
   }
 
 
+  /**
+   * On fait une transformation pour créer un nouveau DataSet de vol locaux
+   */
   val localDataSet = flightDS.filter(vol => localFlight(vol))
 
   localDataSet.show()
 
+  // On fait une autre transformation pour mettre en Upper Case les deux première colonnes.
   val upperDS = flightDS
    .map(vol => Flight(vol.DEST_COUNTRY_NAME.toUpperCase,
                       vol.ORIGIN_COUNTRY_NAME.toUpperCase(),
                       vol.count))
   upperDS.printSchema()
   upperDS.show()
-println("------------ select on DataSet ---------------")
+
+  /**
+   * Depuis Spark 2, les APIs DataSet et DataFrame étant unifiées, on peut utiliser des select sur les DataSet de la même maniere que sur les DataFrame
+   */
   upperDS.select("DEST_COUNTRY_NAME", "ORIGIN_COUNTRY_NAME","count").printSchema()
   upperDS.select("DEST_COUNTRY_NAME", "ORIGIN_COUNTRY_NAME","count").show()
-
 
 
 }
